62cc748ba7e7ccb9dbdcad80ad9c47c849d97b59
KARSPA <kaspar.ivan@outlook.com>
2024-07-24 12:18:53 +0200

    Partie 4

--- routes/routes.js
+++ routes/routes.js
@@ -22,7 +22,11 @@ function setupRoutes(app){
 
         const articles = await Article.find();
 
-        return res.json(articles);
+        return res.json({
+            code : "200",
+            message : "La liste des articles a été récupérées avec succès.",
+            data : articles
+        });
     });
     
     app.get('/article/:articleId', async (req, res) => {
@@ -32,10 +36,18 @@ function setupRoutes(app){
         const foundArticle = await Article.findOne({uid : articleId});
 
         if(!foundArticle){
-            return res.json({message : `Aucun article trouvé avec cet identifiant !`})
+            return res.json({
+                code : "702",
+                message : `Aucun article trouvé avec cet identifiant !`,
+                data : null
+            })
         }
 
-        return res.json(foundArticle);
+        return res.json({
+            code : "200",
+            message : "Article récupéré avec succès.",
+            data : foundArticle
+        });
 
     });
     
@@ -43,12 +55,58 @@ function setupRoutes(app){
 
         //Récup l'article envoyé en JSON
         const articleJSON = req.body;
+        let isTitleOk = await checkTitle(articleJSON);
 
-        articleJSON.uid = uuidv4();
 
-        const createdArticle = await Article.create(articleJSON);
-        
-        return res.json(createdArticle);
+        if(!articleJSON.uid){ //Si pas de champ uid dans articleJSON, alors c'est qu'on créer un article
+            
+            articleJSON.uid = uuidv4();
+    
+            if(!isTitleOk){
+                return res.json({
+                    code : "701",
+                    message : "Impossible d'ajouter un article avec un titre déjà existant.",
+                    data : null
+                });
+            }
+    
+            const createdArticle = await Article.create(articleJSON);
+            
+            return res.json({
+                code : "200",
+                message : "Article ajouté avec succès.",
+                data : createdArticle
+            });
+
+        }else{ // Sinon c'est qu'il y a un champ uid dans articleJSON, donc on modifie
+            let isUIDOk = await checkUID(articleJSON);
+
+            if(!isUIDOk){
+                return res.json({
+                    code : "702",
+                    message : "Impossible de modifier un article dont l'id n'existe pas.",
+                    data : null
+                });
+            }
+
+            if(!isTitleOk){
+                return res.json({
+                    code : "701",
+                    message : "Impossible de modifier un article si un autre article possède le titre choisi. ",
+                    data : null
+                });
+            }
+
+            const modifiedArticle = await Article.updateOne(articleJSON);
+
+            return res.json({
+                code : "200",
+                message : "Article modifié avec succès.",
+                data : articleJSON
+            });
+
+        }
+
 
     });
 
@@ -60,13 +118,49 @@ function setupRoutes(app){
         const deletedArticle = await Article.findOneAndDelete({uid : articleId});
 
         if(!deletedArticle){
-            return res.json({message : `Aucun article avec cet identifiant trouvé, suppression impossible !`})
+            return res.json({
+                code : "702",
+                message : "Impossible de supprimé un article dont l'id n'existe pas.",
+                data : null
+            });
         }
 
-        return res.json({message : `Article supprimé avec succès.`});
-
+        return res.json({
+            code : "200",
+            message : "Article supprimé avec succès.",
+            data : deletedArticle
+        });
     });
 
+
+    async function checkTitle(article){
+    
+        if(article!=undefined && article.title){
+            const checkArticle = await Article.findOne({title : article.title});
+    
+            if(checkArticle){ // Si checkArticle n'est pas null, alors il existe déjà un article avec ce titre.
+                return false;
+            }
+            return true;
+    
+        }
+        return true;
+    }
+
+    async function checkUID(article){
+    
+        if(article!=undefined && article.uid){
+            const checkArticle = await Article.findOne({uid : article.uid});
+    
+            if(checkArticle){ // Si checkArticle n'est pas null, alors il existe déjà un article avec cet uid, on peut donc modifier.
+                return true;
+            }
+            return false;
+    
+        }
+        return true;
+    }
 }
+
 
 module.exports = setupRoutes;
