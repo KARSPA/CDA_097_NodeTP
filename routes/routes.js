8790be5af1ede44643a6253d2e5238954d80f582
KARSPA <kaspar.ivan@outlook.com>
2024-07-24 11:21:19 +0200

    Partie 3 TPArticle

--- routes/routes.js
+++ routes/routes.js
@@ -1,91 +1,70 @@
-let articles = require('../bouchon/articles');
+let osef = require('../bouchon/articles');
+
+const mongoose = require('mongoose');
+const {v4 : uuidv4} = require('uuid');
+
 
 function setupRoutes(app){
 
-    app.get('/articles', (req, res) => {
-        return res.json(articles)
+    mongoose.connection.once('open', ()=>{
+        console.log("Connecté à la BDD");
+    });
+    mongoose.connection.on('error', (err)=>{
+        console.log(`Erreur de la BDD : ${err}`);
+    })
+    mongoose.connect("mongodb://localhost:27017/db_articles");
+    
+
+    const Article = mongoose.model('Article', {uid: String, title: String, content: String, author: String }, 'articles');
+
+
+    app.get('/articles', async (req, res) => {
+
+        const articles = await Article.find();
+
+        return res.json(articles);
     });
     
-    app.get('/article/:articleId', (req, res) => {
-        //res.send(`Retournera l'article ayant l'id ${req.params.articleId}`)
-        
-        const id = parseInt(req.params.articleId)
+    app.get('/article/:articleId', async (req, res) => {
 
-        const foundArticle = articles.find(article => article.id === id);
+        const articleId = req.params.articleId
+
+        const foundArticle = await Article.findOne({uid : articleId});
+
+        if(!foundArticle){
+            return res.json({message : `Aucun article trouvé avec cet identifiant !`})
+        }
 
         return res.json(foundArticle);
 
-        /* MA VERSION
-        const articleId = req.params.articleId;
-        let article = articles.filter(article => article.id == articleId);
-        
-        res.json({article : article});
-        */
     });
     
-    app.post('/save-article', function (req, res) {
-        //res.send('Va créer/mettre à jour un article envoyé')
-
+    app.post('/save-article', async (req, res) => {
 
         //Récup l'article envoyé en JSON
         const articleJSON = req.body;
 
-        let foundArticle = null;
-        if(articleJSON.id != undefined || articleJSON.id){ //Si l'articleJSON possède un id.
+        articleJSON.uid = uuidv4();
 
-            //Essayer de trouver un article existant
-            foundArticle = articles.find(article => article.id === articleJSON.id);
-
-        }
-
-        //Si je trouve, modifier
-        if(foundArticle){
-            for (let property in foundArticle) {
-                foundArticle[property] = articleJSON[property];
-            }
-            return res.json(`L'article avec l'id ${foundArticle.id} a était modifié avec succès.`);
-        }
-
-        //Sinon par défaut j'ajoute l'article au tableau. (attention pour l'instant sans id)
-        articles.push(articleJSON);
-
-        return res.json(`Article créer avec succès`)
-
-        /* MA VERSION
-        let articleId = req.body.id;
-        let article = req.body;
+        const createdArticle = await Article.create(articleJSON);
         
-        if(articleId > articles.length){
-            articles.push(article);
-            res.send(`L'article avec l'id ${articleId} a était créer avec succès.`)
-        }else{
-            articles[articleId-1] = article;
-            res.send(`L'article avec l'id ${articleId} a était mis à jour avec succès.`)
-        }
-        */
+        return res.json(createdArticle);
+
     });
+
     
-    app.delete('/article/:articleId', function(req, res) {
-        const articleId  = parseInt(req.params.articleId);
+    app.delete('/article/:articleId', async (req, res) => {
 
-        const foundArticleIndex = articles.findIndex(article => article.id === articleId)
+        const articleId = req.params.articleId;
 
-        if(foundArticleIndex == -1){
-            return res.json("Article non trouvé.");
+        const deletedArticle = await Article.findOneAndDelete({uid : articleId});
+
+        if(!deletedArticle){
+            return res.json({message : `Aucun article avec cet identifiant trouvé, suppression impossible !`})
         }
 
-        articles.splice(foundArticleIndex, 1);
-        return res.json(`Article avec l'id ${articleId} supprimé avec succès.`)
+        return res.json({message : `Article supprimé avec succès.`});
 
-
-        /* MA VERSION
-        if(articles.find(article => article.id == articleId)==undefined){
-            res.json({message : "Article non trouvé."})
-        }else{
-            articles = articles.findIndex(article => article.id != articleId)
-            res.json({message : `Article avec l'id ${articleId} supprimé avec succès.`})
-        }
-        */
     });
 
 }
